f88d47c8e8b07689221cb14b2f803f7d6b61fde9
buildagent <buildagent@ab-inbev.visualstudio.com>
2018-10-09 16:45:16 +0000

    Only the required directories are pushed.

---
+++ DS.CodeSamples/DS.CodeSamples.Progressbar/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+import { enableRipple } from "@syncfusion/ej2-base";
+enableRipple(true);
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="mt-8 Comp">
+        <div className="flex items-center">
+          <div className="pr-16 font-bold">Progressbar</div>
+          <div className="flex-1 px-2">
+            <div
+              className="e-upload e-control e-keyboard"
+              style={{ border: 0 }}
+            >
+              <ul className="e-upload-files" style={{ border: 0 }}>
+                <li
+                  className="e-upload-file-list"
+                  style={{ border: 0, minHeight: 0 }}
+                >
+                  <span className="e-file-container">
+                    <span className="e-upload-progress-wrap">
+                      <span className="e-progress-inner-wrap">
+                        <progressbar
+                          value="0"
+                          max="100"
+                          className="e-upload-progress-bar e-upload-progress"
+                          style={{ width: 50 + "%" }}
+                        />
+                      </span>
+                    </span>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
