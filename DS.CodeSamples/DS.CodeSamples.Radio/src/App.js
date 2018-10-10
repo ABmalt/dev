99c9c7140d9a29f7deb361c4387f855c8e83135b
buildagent <buildagent@example.com>
2018-10-10 15:59:33 +0000

    Only the required directory is pushed.

--- DS.CodeSamples/DS.CodeSamples.Radio/src/App.js
+++ DS.CodeSamples/DS.CodeSamples.Radio/src/App.js
@@ -1,78 +1,15 @@
 import React, { Component } from "react";
+import { RadioButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { enableRipple } from "@syncfusion/ej2-base";
 enableRipple(true);
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="radio-control">
-          <h4 className="row">Select a payment method</h4>
-          <div className="row">
-            <div className="e-radio-wrapper">
-              <input
-                id="radio1"
-                type="radio"
-                class="e-control e-radio"
-                name="payment"
-                value="credit/debit"
-              />
-              <label for="radio1">
-                <span class="e-ripple-container" data-ripple="true" />
-                <span class="e-label">Credit/Debit Card</span>
-              </label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="e-radio-wrapper">
-              <input
-                id="radio2"
-                type="radio"
-                class="e-control e-radio"
-                name="payment"
-                value="netbanking"
-              />
-              <label for="radio2">
-                <span class="e-ripple-container e-ripple" data-ripple="true">
-                  <div class="e-ripple-element" />
-                </span>
-                <span class="e-label">Net Banking</span>
-              </label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="e-radio-wrapper">
-              <input
-                id="radio3"
-                type="radio"
-                class="e-control e-radio"
-                name="payment"
-                value="cashondelivery"
-              />
-              <label for="radio3">
-                <span class="e-ripple-container" data-ripple="true" />
-                <span class="e-label">Cash on Delivery</span>
-              </label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="e-radio-wrapper">
-              <input
-                id="radio4"
-                type="radio"
-                class="e-control e-radio"
-                name="payment"
-                value="others"
-              />
-              <label for="radio4">
-                <span class="e-ripple-container" data-ripple="true" />
-                <span class="e-label">Other Wallets</span>
-              </label>
-            </div>
-          </div>
-        </div>
+      <div>
+        <RadioButtonComponent checked={true} label="a" name="r" value="a" />
+        <br />
+        <RadioButtonComponent label="b" name="r" value="b" />
       </div>
     );
   }
 }
-
-export default App;
