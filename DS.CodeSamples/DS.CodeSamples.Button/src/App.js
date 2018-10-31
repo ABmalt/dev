c0a84d1690707c094c3ace15fa5e560f4967c6ce
buildagent <buildagent@example.com>
2018-10-31 19:33:30 +0000

    Only the required directory is pushed.

--- DS.CodeSamples/DS.CodeSamples.Button/src/App.js
+++ DS.CodeSamples/DS.CodeSamples.Button/src/App.js
@@ -1,139 +1,14 @@
 import React, { Component } from "react";
-
-import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
-
+import ButtonDoc from "./comps/button";
 import { enableRipple } from "@syncfusion/ej2-base";
 enableRipple(true);
-
-// To render Button.
-export default class ButtonDoc extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Buttons</h1>
-        <div className="mt-8 Comp">
-          <div className="md:flex">
-            <div className="flex-1 px-2">
-              <ButtonComponent cssClass="e-primary">Primary</ButtonComponent>
-              <span>&nbsp;</span>
-              <ButtonComponent cssClass="e-primary" disabled>
-                Primary
-              </ButtonComponent>
-            </div>
-            <div className="flex-1 px-2">
-              <ButtonComponent cssClass="e-outline">Outline</ButtonComponent>
-              <span>&nbsp;</span>
-              <ButtonComponent cssClass="e-outline" disabled>
-                Outline
-              </ButtonComponent>
-            </div>
-            <div className="flex-1 px-2">
-              <ButtonComponent cssClass="e-flat">Flat</ButtonComponent>
-              <span>&nbsp;</span>
-              <ButtonComponent cssClass="e-flat" disabled>
-                Flat
-              </ButtonComponent>
-            </div>
-          </div>
-        </div>
-        <div className="mt-8 Comp">
-          <div className="md:flex">
-            <div className="flex-1 px-2">
-              <ButtonComponent
-                cssClass="e-round e-primary"
-                iconCss="material-icons group_work"
-              />
-              <span>&nbsp;</span>
-              <ButtonComponent
-                cssClass="e-round e-primary"
-                iconCss="material-icons group_work"
-                disabled
-              />
-            </div>
-            <div className="flex-1 px-2">
-              <ButtonComponent
-                cssClass="e-round e-outline"
-                iconCss="material-icons group_work"
-              />
-              <span>&nbsp;</span>
-              <ButtonComponent
-                cssClass="e-round e-outline"
-                iconCss="material-icons group_work"
-                disabled
-              />
-            </div>
-            <div className="flex-1 px-2">
-              <ButtonComponent
-                cssClass="e-round e-flat"
-                iconCss="material-icons group_work"
-              />
-              <span>&nbsp;</span>
-              <ButtonComponent
-                cssClass="e-round e-flat"
-                iconCss="material-icons group_work"
-                disabled
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="mt-8 Comp">
-          <div className="md:flex">
-            <div className="flex-1 px-2">
-              <ButtonComponent
-                cssClass="e-primary"
-                iconCss="material-icons group_work"
-              >
-                Play
-              </ButtonComponent>
-              <span>&nbsp;</span>
-              <ButtonComponent
-                cssClass="e-primary"
-                iconCss="material-icons group_work"
-                disabled
-              >
-                Play
-              </ButtonComponent>
-            </div>
-            <div className="flex-1 px-2">
-              <ButtonComponent
-                cssClass="e-outline"
-                iconCss="material-icons group_work"
-                iconPosition="Right"
-              >
-                Open
-              </ButtonComponent>
-              <span>&nbsp;</span>
-              <ButtonComponent
-                cssClass="e-outline"
-                iconCss="material-icons group_work"
-                iconPosition="Right"
-                disabled
-              >
-                Open
-              </ButtonComponent>
-            </div>
-            <div className="flex-1 px-2">
-              <ButtonComponent
-                cssClass="e-flat"
-                iconCss="material-icons group_work"
-                iconPosition="Right"
-              >
-                Open
-              </ButtonComponent>
-              <span>&nbsp;</span>
-              <ButtonComponent
-                cssClass="e-flat"
-                iconCss="material-icons group_work"
-                iconPosition="Right"
-                disabled
-              >
-                Open
-              </ButtonComponent>
-            </div>
-          </div>
-        </div>
+      <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 mb-64 pb-64">
+        <ButtonDoc />
       </div>
     );
   }
 }
+export default App;
---
+++ DS.CodeSamples/DS.CodeSamples.Button/src/comps/button.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+
+export default class ButtonDoc extends Component {
+  render() {
+    return (
+      <div>
+        <div className="w-48 font-bold">Button</div>
+        <div className="mt-8 Comp">
+          <div className="md:flex items-center">
+            <div className="flex-1 px-2">
+              <ButtonComponent cssClass="e-primary">Primary</ButtonComponent>
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-primary"
+                iconCss="e-icons e-add-icon"
+              />
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-primary"
+                iconCss="e-icons e-add-icon"
+                disabled
+              />
+            </div>
+            <div className="flex-1 px-2">
+              <ButtonComponent cssClass="e-outline">Outline</ButtonComponent>
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-outline"
+                iconCss="e-icons e-add-icon"
+              />
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-outline"
+                iconCss="e-icons e-add-icon"
+                disabled
+              />
+            </div>
+            <div className="flex-1 px-2">
+              <ButtonComponent cssClass="e-flat">Flat</ButtonComponent>
+              <span>&nbsp;</span>
+              <ButtonComponent cssClass="e-flat" iconCss="e-icons e-add-icon" />
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-flat"
+                iconCss="e-icons e-add-icon"
+                disabled
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8 Comp">
+          <div className="md:flex items-center">
+            <div className="flex-1 px-2">
+              <ButtonComponent
+                cssClass="e-primary"
+                iconCss="e-icons e-play-icon"
+              >
+                Play
+              </ButtonComponent>
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-primary"
+                iconCss="e-icons e-play-icon"
+                disabled
+              >
+                Play
+              </ButtonComponent>
+            </div>
+            <div className="flex-1 px-2">
+              <ButtonComponent
+                cssClass="e-outline"
+                iconCss="e-icons e-open-icon"
+                iconPosition="Right"
+              >
+                Open
+              </ButtonComponent>
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-outline"
+                iconCss="e-icons e-open-icon"
+                iconPosition="Right"
+                disabled
+              >
+                Open
+              </ButtonComponent>
+            </div>
+            <div className="flex-1 px-2">
+              <ButtonComponent
+                cssClass="e-flat"
+                iconCss="e-icons e-open-icon"
+                iconPosition="Right"
+              >
+                Open
+              </ButtonComponent>
+              <span>&nbsp;</span>
+              <ButtonComponent
+                cssClass="e-flat"
+                iconCss="e-icons e-open-icon"
+                iconPosition="Right"
+                disabled
+              >
+                Open
+              </ButtonComponent>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
