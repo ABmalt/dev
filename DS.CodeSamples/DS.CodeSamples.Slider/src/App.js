1315f060ba1c60c5b0a44d26033b5769e1e5e2a6
buildagent <buildagent@example.com>
2018-11-02 22:04:45 +0000

    Only the required directory is pushed.

--- DS.CodeSamples/DS.CodeSamples.Slider/src/App.js
+++ DS.CodeSamples/DS.CodeSamples.Slider/src/App.js
@@ -1,41 +1,15 @@
 import React, { Component } from "react";
-import { SliderComponent } from "@syncfusion/ej2-react-inputs";
+
+import SliderDoc from "./comps/slider";
 import { enableRipple } from "@syncfusion/ej2-base";
 enableRipple(true);
-export default class App extends Component {
-  constructor() {
-    super(...arguments);
-    // Initialize ticks with placement, largestep, smallstep
-    this.defaultTicks = {
-      placement: "Before",
-      largeStep: 0.1,
-      smallStep: 0.05,
-      showSmallTicks: true
-    };
-    this.defaultTooltip = {
-      placement: "Before",
-      isVisible: true,
-      showOn: "Focus"
-    };
-    this.fields = { value: "value", text: "text" };
-  }
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Sliders</h1>
-        <SliderComponent />
-        <br />
-        <br />
-        <SliderComponent
-          value={0.3}
-          min={0.1}
-          max={0.9}
-          step={0.05}
-          ticks={this.defaultTicks}
-          showButtons={true}
-          tooltip={this.defaultTooltip}
-        />
+      <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 mb-64 pb-64">
+        <SliderDoc />
       </div>
     );
   }
 }
+export default App;
---
+++ DS.CodeSamples/DS.CodeSamples.Slider/src/comps/slider.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { SliderComponent } from "@syncfusion/ej2-react-inputs";
+
+export default class SilderDoc extends Component {
+  constructor() {
+    super(...arguments);
+    // Initialize ticks with placement, largestep, smallstep
+    this.defaultTicks = {
+      placement: "Before",
+      largeStep: 20,
+      smallStep: 10,
+      showSmallTicks: true
+    };
+    this.defaultTooltip = {
+      placement: "Before",
+      isVisible: true,
+      showOn: "Focus"
+    };
+    this.fields = { value: "value", text: "text" };
+  }
+  render() {
+    return (
+      <div className="mt-8 Comp">
+        <div className="md:flex items-center">
+          <div className="w-48 font-bold">Slider</div>
+          <div className="flex-1 text-center px-2">
+            <SliderComponent cssClass="abi-css" />
+          </div>
+          <div className="flex-1 text-center px-2">
+            <SliderComponent
+              value={45}
+              min={0}
+              max={100}
+              step={5}
+              ticks={this.defaultTicks}
+              showButtons={true}
+              tooltip={this.defaultTooltip}
+            />
+          </div>
+          <div className="flex-1 text-center px-2">
+            <SliderComponent
+              value={45}
+              min={0}
+              max={100}
+              step={5}
+              ticks={this.defaultTicks}
+              tooltip={this.defaultTooltip}
+              enabled={false}
+              cssClass="malt-style"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
