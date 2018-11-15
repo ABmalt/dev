f595c1cf8e568df0a8618f74ec23c3d474a23f5e
buildagent <buildagent@example.com>
2018-11-15 18:55:06 +0000

    Only the required directory is pushed.

--- DS.CodeSamples/DS.CodeSamples.Sidebar/src/comps/sidebar.js
+++ DS.CodeSamples/DS.CodeSamples.Sidebar/src/comps/sidebar.js
@@ -1,22 +1,48 @@
 import React, { Component } from "react";
-// import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 export default class SidebarDoc extends Component {
+  constructor() {
+    super(...arguments);
+    this.sidebarobj = SidebarComponent;
+  }
+  rendereComplete() {
+    this.sidebarobj.closeOnDocumentClick = true;
+    this.sidebarobj.showBackdrop = true;
+    this.sidebarobj.type = "Over";
+  }
+  componentDidMount() {
+    this.rendereComplete();
+  }
+  openClick() {
+    this.sidebarobj.show();
+  }
+  closeClick() {
+    this.sidebarobj.hide();
+  }
   render() {
     return (
-      <div className="control-section">
-        <div id="wrapper">
-          <SidebarComponent
-            id="default-sidebar"
-            ref={Sidebar => (this.sidebarobj = Sidebar)}
-          >
-            <div className="title">Sidebar content</div>
-          </SidebarComponent>
-          <div>
-            <div className="title">Main content</div>
-            <div className="sub-title">content goes here</div>
+      <div>
+        <span
+          id="hamburger"
+          className="e-icons"
+          onClick={this.openClick.bind(this)}
+        />
+        <SidebarComponent
+          ref={Sidebar => (this.sidebarobj = Sidebar)}
+          type="Over"
+          width="304px"
+        >
+          <div className="sidebar-hdr">
+            <div className="sidebar-title">Sidebar</div>
+            <div
+              id="close"
+              className="e-icons"
+              onClick={this.closeClick.bind(this)}
+            />
           </div>
-        </div>
+          <div className="sidebar-content">List</div>
+        </SidebarComponent>
+        <section className="main">App content</section>
       </div>
     );
   }
